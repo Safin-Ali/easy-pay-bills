cc7d0f7bf607f4960d6299bd6a8f5cb36ae0269f
Safin Ali <safin.ali.7205@gmail.com>
2023-01-29 02:39:28 +0600

    created Navbar Component

---
+++ src/Components/Appbar/Appbar.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Appbar = () => {
+    return (
+        <header>
+            <nav className={`flex md:px-[5%] py-[2%] gap-3 lg:py-[1%] shadow justify-between items-center`}>
+                <div className={`flex gap-2 items-center`}>
+                    {/* Brand Name */}
+                    <div className={`px-[2.5%] md:px-0`}>
+                        <h2 className={`text-lg md:text-3xl dark:text-whitePrimary font-bold font-mincho`}>Logo</h2>
+                    </div>
+                </div>
+                <div>
+                    <h5>Total paid</h5>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Appbar;
