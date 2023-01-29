f3ab4a85f39e6bbb1b7d6f1a777ff5be9f3e913f
Safin Ali <safin.ali.7205@gmail.com>
2023-01-29 21:52:34 +0600

    always check userLoad & page load

---
+++ src/page/Private/PrivatePage.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { DataContext } from '../../Context/DataProv';
+
+const PrivatePage = ({children}) => {
+    const {userLoad,loaded} = useContext(DataContext);
+    if(!loaded) return <p>Wait</p>
+    if(!userLoad) return <Navigate to={'/login'}></Navigate>;
+    return children;
+};
+
+export default PrivatePage;
