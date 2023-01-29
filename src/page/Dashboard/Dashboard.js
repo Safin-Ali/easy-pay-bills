94f4c0f1caf9efeaaf13e2853c1750cc2afc148e
Safin Ali <safin.ali.7205@gmail.com>
2023-01-30 01:11:21 +0600

    new clean way table compo implement

--- src/page/Dashboard/Dashboard.js
+++ src/page/Dashboard/Dashboard.js
@@ -1,15 +1,22 @@
 import React, { useContext } from 'react';
-import Table from '../../Components/Table/Table';
 import {BsSearch} from 'react-icons/bs';
 import PrimaryButton from '../../Components/Button/PrimaryButton';
 import { DataContext } from '../../Context/DataProv';
+import TableHead from '../../Components/Table/TableHead';
+import TableRow from '../../Components/Table/TableRow';
 
 const Dashboard = () => {
 
-    const {toggleModal,setToggleModal} = useContext(DataContext);
+    const {toggleModal,setToggleModal,paidBillsData,setPaidBillsData} = useContext(DataContext);
+
+    console.log(paidBillsData?.data?.slice(0,10))
+
+    const tableHead = ["bill id", "full name","email","phone","paid amount"];
 
     return (
         <section className={`m-[2%]`}>
+
+            {/* top section */}
             <div>
                 <div className={`flex justify-between gap-5 bg-white border rounded-tl-lg rounded-tr-lg p-2 items-center my-4 shadow-md`}>
                     <div className={`flex items-center gap-5`}>
@@ -30,7 +37,19 @@ const Dashboard = () => {
                     <PrimaryButton onClick={()=>setToggleModal({...toggleModal,action:'post',stateBool: true})}>Add New Bill</PrimaryButton>
                 </div>
             </div>
-            <Table></Table>
+
+            {/* table section */}
+            <div className={`w-full`}>
+
+                <table className={`w-full`}>
+                    <tbody>
+                        <TableHead val={tableHead}></TableHead>
+                        <TableRow val={paidBillsData?.data?.slice(0,10)}></TableRow>
+                    </tbody>
+                </table>
+
+            </div>
+
         </section>
     );
 };
