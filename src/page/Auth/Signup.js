ccd80f8cc3487194bd51116d4dfb5c0d82525b2a
Safin Ali <safin.ali.7205@gmail.com>
2023-01-29 15:33:56 +0600

    created simple login and registration page

--- src/page/Auth/Signup.js
+++ src/page/Auth/Signup.js
@@ -4,29 +4,37 @@ import {IoMdEye,IoMdEyeOff} from 'react-icons/io';
 import handleDynaForm from '../../Hooks/handleDynaForm';
 import axios from 'axios';
 
-const Login = () => {
+const Signup = () => {
 
     // togglr pass feild
     const [passToggle,setPassToggle] = useState(false);
 
     // navigator
+
     const navigate = useNavigate();
 
     // post form value;
-    const handleLogin = async (obj,clearForm) => {
-        const res = await axios.post(`http://localhost:5000/login`,obj);
-        if(res.data.acknowledge) {
-            localStorage.setItem('authData',res.data.encodedUserInfo);
-            alert('login successful');
-            clearForm();
-            navigate('/');
+
+    const handleSignup = async (obj,clearForm) => {
+        const res = await axios.post(`http://localhost:5000/registration`,obj);
+
+        if(res.data.acknowledged) {
+            window.alert('Registration Successful');
+            clearForm()
+            return navigate('/login');
         }
+
     };
 
     return (
         <div className={`w-[30%] mx-auto flex items-center justify-center h-[calc(100vh-40px)]`}>
-            <form onSubmit={handleDynaForm(handleLogin)} className={`shadow border p-5 w-full`}>
-                <h4 className={`text-xl text-center font-bold underline`}>LOGIN</h4>
+            <form onSubmit={handleDynaForm(handleSignup)} className={`shadow border p-5 w-full`}>
+                <h4 className={`text-xl text-center font-bold underline`}>Registration</h4>
+
+                <div className={`my-5`}>
+                    <p className={`font-medium ml-1 mb-2`}>Full Name</p>
+                    <input type="text" name={'userFullName'} placeholder={"enter email"} className={`border p-2 w-full rounded`} />
+                </div>
 
                 <div className={`my-5`}>
                     <p className={`font-medium ml-1 mb-2`}>Email</p>
@@ -34,7 +42,7 @@ const Login = () => {
                 </div>
 
                 <div className={`my-5`}>
-                    <p className={`font-medium ml-1 mb-2`}>Password</p>
+                    <p className={`font-medium ml-1 mb-2`}>Confirm Password</p>
                     <div className={`relative`}>
                         <input type={passToggle ? 'text' : 'password'} name={'userPass'} placeholder={"enter password"} className={`border p-2 w-full rounded`} />
                         {
@@ -43,9 +51,15 @@ const Login = () => {
                         }
                     </div>
                 </div>
+
+                <div className={`my-5`}>
+                    <p className={`font-medium ml-1 mb-2`}>Password</p>
+                        <input type={passToggle ? 'text' : 'password'} name={'confirmPass'} placeholder={"confirm password"} className={`border p-2 w-full rounded`} />
+                </div>
+
                 <div className={`flex justify-between items-center`}>
-                    <div><button className={`bg-blue-600 py-1 px-2 rounded-md text-white hover:bg-blue-700 duration-200`}>SIGNIN</button></div>
-                    <Link className={'text-blue-600'} to={'/signup'}>create account</Link>
+                    <div><button className={`bg-blue-600 py-1 px-2 rounded-md text-white hover:bg-blue-700 duration-200`}>SIGNUP</button></div>
+                    <Link className={'text-blue-600'} to={'/login'}>signin your account</Link>
                 </div>
             </form>
 
@@ -53,4 +67,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Signup;
