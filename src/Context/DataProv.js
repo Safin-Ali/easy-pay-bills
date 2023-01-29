7f8aa6dd8602592520c9ab6dff3f7cfc67f2bf41
Safin Ali <safin.ali.7205@gmail.com>
2023-01-29 21:17:11 +0600

    auto fetch exist user data

--- src/Context/DataProv.js
+++ src/Context/DataProv.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const DataContext = createContext();
 
@@ -12,18 +13,49 @@ const DataProv = ({children}) => {
     const [toggleModal,setToggleModal] = useState(modalInitVal);
     const closeToggle = {...toggleModal,stateBool: false,action: ''};
 
+    // get existUser
+    const[userLoad,setUserLoad] = useState(null);
+    const[loaded,setLoaded] = useState(false);
+
+    // logout function
+    const logout = () => {
+        setUserLoad(false);
+        localStorage.removeItem('authData');
+    }
+
+    useEffect(()=>{
+        const userEncodedInfo = localStorage.getItem('authData');
+        const source = axios.CancelToken.source();
+
+        userEncodedInfo && axios.get(`https://online-payment-bills-server.vercel.app/checkUser`,{headers: {secretKey: userEncodedInfo}})
+        .then(res => {
+            setLoaded(true);
+            if(res.data.acknowledge) return setUserLoad(true);
+        })
+        .catch(e => {
+            setLoaded(undefined);
+            return console.log(e.message);
+        });
+
+        return () => source.cancel();
+    },[]);
+
     const providingFunc = {
         toggleModal,
         setToggleModal,
-        closeToggle
-    }
+        closeToggle,
+        loaded,
+        userLoad,
+        logout
+    };
+
     return(
         <DataContext.Provider value={providingFunc}>
             {
                 children
             }
         </DataContext.Provider>
-    )
+    );
 };
 
 export default DataProv;
